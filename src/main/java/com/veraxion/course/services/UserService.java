d2849b372e60e8071d0f17443f293de784e5230d
Tiago Moraes <tgpmoraes@gmail.com>
2025-02-01 16:03:18 -0300

    Service layer, component registration

---
+++ src/main/java/com/veraxion/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.veraxion.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.veraxion.course.entities.User;
+import com.veraxion.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
