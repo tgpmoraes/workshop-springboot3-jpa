5a68c666584a27eb990cbe795462a3197cef62d0
Tiago Gomes Pessoa de Moraes <tgpmoraes@gmail.com>
2025-02-08 17:00:55 -0300

    Exception handling - update

--- src/main/java/com/veraxion/course/services/UserService.java
+++ src/main/java/com/veraxion/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.veraxion.course.repositories.UserRepository;
 import com.veraxion.course.services.exceptions.DatabaseException;
 import com.veraxion.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(e.getMessage());
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
